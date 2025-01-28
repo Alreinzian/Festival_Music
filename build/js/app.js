0ffde410a8ac52f0ae40c6b77f8812d518ff6122
Daniel Manrique <sagender1623@gmail.com>
2025-01-27 22:52:49 -0500

    Galería terminada

--- build/js/app.js
+++ build/js/app.js
@@ -10,5 +10,37 @@ function crearGaleria() {
     imagen.src = `src/img/gallery/full/${i}.jpg`;
     imagen.alt = "Imagen Galeria";
     galeria.appendChild(imagen);
+
+    //Event Handler es un evento que responde a una acción
+    imagen.onclick = function () {
+      mostrarImagen(i);
+    };
+    galeria.appendChild(imagen);
   }
+}
+function mostrarImagen(i) {
+  const imagen = document.createElement("IMG");
+  imagen.src = `src/img/gallery/full/${i}.jpg`;
+  imagen.alt = "Imagen Galeria";
+  galeria.appendChild(imagen);
+  //Generar modal
+  const modal = document.createElement("DIV");
+  modal.classList.add("modal");
+  modal.onclick = cerrarModal;
+  modal.appendChild(imagen);
+
+  //Agregar al HTML
+  const body = document.querySelector("body");
+  body.classList.add('overflow-hidden');
+  body.appendChild(modal);
+}
+function cerrarModal() {
+  const modal = document.querySelector(".modal");
+  modal.classList.add("fade-out");
+
+  setTimeout(() => {
+    modal?.remove();
+    const body = document.querySelector("body");
+  body.classList.remove('overflow-hidden');
+  }, 500);
 }
